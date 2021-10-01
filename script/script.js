9f7c200f4e1d13ade7ae7b38d83a797496fa19e1
Seva Markov <vsevolodmarkoff@yandex.ru>
2021-10-01 14:08:50 +0300

    feat: проверка img на соответствие лайку

--- script/script.js
+++ script/script.js
@@ -1,43 +1,45 @@
 // popup opening and closing
 
-const editButton = document.querySelector('.profile__button-edit');
-const popupWindow = document.querySelector('.popup');
-const popupClose = document.querySelector('.popup__close');
+const editButton = document.querySelector(".profile__button-edit");
+const popupWindow = document.querySelector(".popup");
+const popupClose = document.querySelector(".popup__close");
 
-editButton.addEventListener('click', openPopup);
-popupClose.addEventListener('click', closePopup);
+editButton.addEventListener("click", openPopup);
+popupClose.addEventListener("click", closePopup);
 
 function openPopup() {
-  if (!popupWindow.classList.contains('popup_opened')) {
-    popupWindow.classList.add('popup_opened');
+  if (!popupWindow.classList.contains("popup_opened")) {
+    popupWindow.classList.add("popup_opened");
   }
 }
 
 function closePopup() {
-  if (popupWindow.classList.contains('popup_opened')) {
-    popupWindow.classList.remove('popup_opened');
+  if (popupWindow.classList.contains("popup_opened")) {
+    popupWindow.classList.remove("popup_opened");
   }
 }
 
 //like element
 
-let elementLike = Array.from(document.querySelectorAll('.element__like'));
+let elementLike = Array.from(document.querySelectorAll(".element__like"));
 
 elementLike.forEach((img, index) => {
-  img.addEventListener('click', () => {
-    img.classList.toggle('element__like_active');
-    img.setAttribute('style', 'opacity: 1');
+  img.addEventListener("click", () => {
+    if (img.classList.contains("element__like")) {
+      img.classList.toggle("element__like_active");
+      img.setAttribute("style", "opacity: 1");
+    }
   });
 });
 
 //input to profile
 
-const profileTitle = document.querySelector('.profile__title');
-const progileSubtitle = document.querySelector('.profile__subtitle');
+const profileTitle = document.querySelector(".profile__title");
+const progileSubtitle = document.querySelector(".profile__subtitle");
 
-const formElement = document.querySelector('.popup__form');
-const nameInput = formElement.querySelector('.name-input');
-const jobInput = formElement.querySelector('.job-input');
+const formElement = document.querySelector(".popup__form");
+const nameInput = formElement.querySelector(".name-input");
+const jobInput = formElement.querySelector(".job-input");
 
 if (nameInput.value.length === 0) {
   nameInput.value = profileTitle.textContent;
@@ -54,12 +56,12 @@ function formSubmitHandler(evt) {
   let name = nameInput.value;
 
   if (name.length > 30) {
-    alert('Имя может быть не более 30 симвлов');
-    return
+    alert("Имя может быть не более 30 симвлов");
+    return;
   }
   if (job.length > 50) {
-    alert('Профессия может быть не более 50 симвлов');
-    return
+    alert("Профессия может быть не более 50 симвлов");
+    return;
   }
 
   profileTitle.textContent = name;
@@ -68,4 +70,4 @@ function formSubmitHandler(evt) {
   closePopup();
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener("submit", formSubmitHandler);
