c94c43f4ee77b7c1914e62eb3e49d523ac4f3916
SevaMarkov88 <vsevolodmarkoff@yandex.ru>
2021-10-15 19:14:00 +0300

    newCards added

--- script/script.js
+++ script/script.js
@@ -9,6 +9,35 @@ const formElement = document.querySelector(".popup__form");
 const nameInput = formElement.querySelector(".popup__text_input-type_name");
 const jobInput = formElement.querySelector(".popup__text_input-type_job");
 
+// cards array
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 // Listeners
 
 editButton.addEventListener("click", openPopup);
