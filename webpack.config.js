1b73e6ed49ef911c84909c05e9a7674f3294968f
SevaMarkov88 <vsevolodmarkoff@yandex.ru>
2021-11-30 21:51:41 +0300

    Revert "sprint 8 ready"
    This reverts commit d582775fd38ee63162d8e5bd6299b23c3d9e4034.

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/TestIndex.html"
+      template: "./src/index.html"
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin()
